e6557c50093ad5095ea236faa9001a626891e6cd
Alexandr Lipanov <atovarno@yandex.ru>
2020-08-09 15:49:17 +0400

    calc

--- main/src/Calculator.java
+++ main/src/Calculator.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
         public static void main(String[] args) {
 
             Scanner in = new Scanner(System.in);
+
             System.out.println("Введите первое число");
             int firstNum = in.nextInt();
             System.out.println("Введите второе чсило");
             int secondNum = in.nextInt();
             System.out.println("Введите знак");
             String znak = in.next();
+
             if(znak.equals("+")){
                 System.out.println("Вот сумма этих чисел " + (firstNum + secondNum));
             }else if(znak.equals("-")){
