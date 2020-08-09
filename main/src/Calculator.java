43fcc96a478cd65259441b8a4661d1fcbd493db3
Alexandr Lipanov <atovarno@yandex.ru>
2020-08-09 15:37:19 +0400

    calc

---
+++ main/src/Calculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+    public class Calculator {
+
+        public static void main(String[] args) {
+
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите первое число");
+            int firstNum = in.nextInt();
+            System.out.println("Введите второе чсило");
+            int secondNum = in.nextInt();
+            System.out.println("Введите знак");
+            String znak = in.next();
+            if(znak.equals("+")){
+                System.out.println("Вот сумма этих чисел " + (firstNum + secondNum));
+            }else if(znak.equals("-")){
+                System.out.println("Вот разность этих чисел " + (firstNum - secondNum));
+            }else if(znak.equals("*")){
+                System.out.println("Вот произведение этих чисел " + (firstNum * secondNum));
+            }else if(znak.equals("/")){
+                System.out.println("Вот деление этих чисел " + (firstNum / secondNum));
+            }else{
+                System.out.println("Что то не так ");}
+        }
+    }
